fe8b32de16b18a3c069fd58d84e94f00a4e8f37d
humbertoromanojr <astronomi@gmail.com>
2026-06-20 10:53:16 -0300

    Updating Footer styles

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,23 @@
-import React from "react";
+import { Typography } from "antd";
 
 export default function Footer() {
   return (
     <div className="Footer">
-      <h2>Footer</h2>
+      <Typography.Link href="tel:+5527999998888">
+        +5527999998888
+      </Typography.Link>
+      <Typography.Link
+        href="https://github.com/humbertoromanojr/DailyExpense_IncomeDiary"
+        target="_blank"
+      >
+        Privacy Policy
+      </Typography.Link>
+      <Typography.Link
+        href="https://github.com/humbertoromanojr/DailyExpense_IncomeDiary"
+        target="_blank"
+      >
+        Terms of Use
+      </Typography.Link>
     </div>
   );
 }
