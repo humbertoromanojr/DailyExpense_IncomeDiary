12025ec55847ca7fec1754f44de01f79ee43b0c1
humbertoromanojr <astronomi@gmail.com>
2026-06-22 12:24:09 -0300

    Add Route and Setting body-parser

--- api/server.js
+++ api/server.js
@@ -4,6 +4,16 @@ const app = express();
 // Connect mongoDB
 const fileDB = require("./connection");
 
+// imports the Routes and Models
+const routeAdd = require("./routes/add");
+
+// import body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/add", routeAdd);
+
 app.get("/", (req, res) => {
   res.end("Welcome from server Node.js");
 });
