a80d2049990ef383b8fc5d8d404469d5c13d9e5b
humbertoromanojr <astronomi@gmail.com>
2026-06-23 08:08:44 -0300

    Updating SideMenu component

--- client/src/components/SideMenu/index.js
+++ client/src/components/SideMenu/index.js
@@ -1,9 +1,39 @@
-import React from "react";
+import { Menu } from "antd";
+import {
+  AppstoreOutlined,
+  AppstoreAddOutlined,
+  EditOutlined,
+  OrderedListOutlined,
+} from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 export default function SideMenu() {
+  const navigate = useNavigate();
+
   return (
     <div className="SideMenu">
-      <h2>SideMenu</h2>
+      <Menu
+        onClick={(item) => {
+          navigate(item.key);
+        }}
+        items={[
+          {
+            label: "Dashboard",
+            icon: <AppstoreOutlined />,
+            key: "/",
+          },
+          {
+            label: "Add Items",
+            icon: <AppstoreAddOutlined />,
+            key: "/additem",
+          },
+          {
+            label: "List Items",
+            icon: <OrderedListOutlined />,
+            key: "/mylist",
+          },
+        ]}
+      ></Menu>
     </div>
   );
 }
