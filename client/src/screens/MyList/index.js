471a43a276a218c4ec9796b4d9883f8158a9d4e4
humbertoromanojr <astronomi@gmail.com>
2026-06-23 08:07:51 -0300

    Updating MyList screen

---
+++ client/src/screens/MyList/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { Typography, Flex, Space, Table, Tag } from "antd";
+import axios from "axios";
+
+import Item from "../Item";
+
+export default function MyList() {
+  const [dataItems, setDataItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("api/add/myList")
+      .then((res) => {
+        console.log(res.data.data);
+        setDataItems(res.data.data || res.data || []);
+      })
+      .catch((err) => {
+        console.log(err);
+        setDataItems([]);
+      });
+  }, []);
+
+  //Map the list of items to the “items” object
+  const listItems = dataItems.map((item) => {
+    return (
+      <div>
+        <Item item={item} />
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <Typography.Title level={4}>
+        My list of daily expenses and incomes
+      </Typography.Title>
+      {listItems}
+    </div>
+  );
+}
