84836603b939e4b6a2fdba1b5d38207d6793d8be
humbertoromanojr <astronomi@gmail.com>
2026-06-23 08:06:15 -0300

    Updating Routes

--- client/src/components/AppRoutes/index.js
+++ client/src/components/AppRoutes/index.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "../../screens/Dashboard";
-import Add from "../../screens/Add";
+import AddItem from "../../screens/AddItem";
+import EditItem from "../../screens/EditItem";
 import MyList from "../../screens/MyList";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} exact />
-      <Route path="/add" element={<Add />} exact />
+      <Route path="/addItem" element={<AddItem />} exact />
+      <Route path="/editItem:iditem" element={<EditItem />} exact />
       <Route path="/myList" element={<MyList />} exact />
     </Routes>
   );
