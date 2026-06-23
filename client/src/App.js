5be9ffd2922922e9716128bc77d660b88523bb11
humbertoromanojr <astronomi@gmail.com>
2026-06-23 08:06:56 -0300

    Updating App

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Space } from "antd";
+
+import Header from "./components/Header";
+import SideMenu from "./components/SideMenu";
+import Content from "./components/Content";
+
+import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Space className="SideMenuAndContent">
+        <SideMenu></SideMenu>
+        <Content></Content>
+      </Space>
+      <Footer />
     </div>
   );
 }
